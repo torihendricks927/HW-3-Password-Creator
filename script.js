887f0e193494659fa2e32e1a28f4ed6864cda329
Tori <torihendricks927@yahoo.com>
2022-03-22 22:49:16 -0400

    Beginning portion of js with var and function

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var specialSym = ["!","@","#","$","%","^","&","*","(",")","-","+"]
+var numbersChar = ["1","2","3","4","5","6","7","8","9","0"]
+
+function generatePassword() {
+  var finalPassword = '';
+  var passLength = 
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
